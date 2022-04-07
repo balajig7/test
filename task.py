dd09a70dc3f3fd4b8d9c7d93349c550b7b5bd491
balajig7 <98063581+balajig7@users.noreply.github.com>
2022-04-06 21:48:32 -0400

    Update task.py

--- task.py
+++ task.py
@@ -18,7 +18,7 @@ import time
 
 
 # Use WebDriver to open a Chrome tab and navigate to Instagram login page
-chromedriver_path = "/usr/lib/chromium-browser/chromedriver"
+chromedriver_path = "/usr/bin/chromedriver"
 webdriver = webdriver.Chrome(executable_path = chromedriver_path)
 webdriver.get("https://www.instagram.com/accounts/login")
 sleep(1)
