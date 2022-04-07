4ecd04bfc7a71a3fd8ade61aa7efdc42875a2c9a
balajig7 <98063581+balajig7@users.noreply.github.com>
2022-04-06 22:06:37 -0400

    Update task.py

--- task.py
+++ task.py
@@ -15,6 +15,11 @@ import time
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
 chrome_options = Options()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
